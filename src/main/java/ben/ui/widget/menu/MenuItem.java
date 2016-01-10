dc3a1382ba9a88bbaad7a595ca45dc66f4c8319f
Ben Smith <bensmith87@gmail.com>
2016-01-10 12:42:20 +1100

    Java 8

---
+++ src/main/java/ben/ui/widget/menu/MenuItem.java
@@ -0,0 +1,7 @@
+package ben.ui.widget.menu;
+
+/**
+ * Created by ben on 9/01/16.
+ */
+public class MenuItem {
+}
---
+++ src/main/java/ben/ui/widget/window/IWindow.java
@@ -0,0 +1,7 @@
+package ben.ui.widget.window;
+
+/**
+ * Created by ben on 10/01/16.
+ */
+public interface IWindow {
+}
