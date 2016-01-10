3ca04493b24fa5b54796598e3193dfc96396f54a
Ben Smith <bensmith87@gmail.com>
2016-01-10 12:54:28 +1100

    Remove is now designed for extension

--- src/main/java/ben/ui/widget/window/IWindow.java
+++ src/main/java/ben/ui/widget/window/IWindow.java
@@ -1,7 +1,20 @@
 package ben.ui.widget.window;
 
+import ben.ui.math.Rect;
+import ben.ui.widget.IWidget;
+
+import javax.annotation.Nonnull;
+
 /**
- * Created by ben on 10/01/16.
+ * Window Interface.
  */
-public interface IWindow {
+public interface IWindow extends IWidget {
+
+    /**
+     * Set the desktop rectangle.
+     *
+     * Can be used to clip the windows position.
+     * @param rect the desktop rectangle
+     */
+    void setDesktopRect(@Nonnull Rect rect);
 }
--- src/main/java/ben/ui/widget/window/TitleBar.java
+++ src/main/java/ben/ui/widget/window/TitleBar.java
@@ -114,13 +114,12 @@ public final class TitleBar extends AbstractWidget {
     }
 
     @Override
-    public void remove(@Nonnull GL2 gl) {
+    protected void preRemove(@Nonnull GL2 gl) {
         if (backgroundRenderer != null) {
             backgroundRenderer.remove(gl);
         }
         if (textRenderer != null) {
             textRenderer.remove(gl);
         }
-        super.remove(gl);
     }
 }
