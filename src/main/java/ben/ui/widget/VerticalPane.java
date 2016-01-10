e37273d25f4210fd92b3baf0d5df7c893bc3d420
Ben Smith <bensmith87@gmail.com>
2016-01-10 12:52:08 +1100

    Renamed padding and border to be inline with the CSS box model names and remove is now designed for extension

--- src/main/java/ben/ui/widget/VerticalPane.java
+++ src/main/java/ben/ui/widget/VerticalPane.java
@@ -1,9 +1,7 @@
 package ben.ui.widget;
 
 import ben.ui.math.PmvMatrix;
-import ben.ui.renderer.FlatRenderer;
 import ben.ui.resource.GlResourceManager;
-import ben.ui.resource.color.Color;
 import ben.ui.math.Vec2i;
 import javax.annotation.Nonnull;
 
@@ -13,7 +11,7 @@ import javax.annotation.Nullable;
 /**
  * Vertical Pane.
  *
- * Lays widgets out in a vertical column, with padding between them and a border around them.
+ * Lays widgets out in a vertical column, with spacing between them and a padding around them.
  *
  * <pre>
  * +----------+---+
@@ -27,65 +25,45 @@ import javax.annotation.Nullable;
  *
  * The widgets will all keep their preferred size.
  *
- * The pane will have a preferred width equal to the width of the largest child widget plus border.
+ * The pane will have a preferred width equal to the width of the largest child widget plus padding.
  *
- * The pane will have a preferred height equal to the sum of the heights of the child widgets, plus border and padding.
+ * The pane will have a preferred height equal to the sum of the heights of the child widgets, plus spacing and padding.
  */
 public final class VerticalPane extends AbstractPane {
 
     /**
-     * The border between the widgets.
+     * The spacing between the widgets.
+     */
+    private static final int SPACING = 5;
+
+    /**
+     * The width of the padding around the frame.
      */
     private static final int PADDING = 5;
 
     /**
-     * The width of the border.
+     * Is there padding around the frame?
      */
-    private static final int BORDER = 5;
-
-    /**
-     * The background colour of the pane.
-     */
-    @Nonnull
-    private static final Color BACKGROUND_COLOR = new Color(0.235f, 0.247f, 0.254f);
-
-    /**
-     * Is there border around the frame?
-     */
-    private final boolean border;
-
-    /**
-     * The background renderer.
-     */
-    @Nullable
-    private FlatRenderer backgroundRenderer;
+    private final boolean padding;
 
     /**
      * Constructor.
      * @param name the name of the pane
-     * @param border is there a border around the frame
+     * @param padding is there padding around the frame
      */
-    public VerticalPane(@Nullable String name, boolean border) {
-        super(name, true);
-        this.border = border;
+    public VerticalPane(@Nullable String name, boolean padding) {
+        super(name, true, true);
+        this.padding = padding;
     }
 
     @Override
-    protected void initDraw(@Nonnull GL2 gl, @Nonnull GlResourceManager glResourceManager) {
-        backgroundRenderer = new FlatRenderer(gl, glResourceManager, getRect(), BACKGROUND_COLOR);
-    }
+    protected void initDraw(@Nonnull GL2 gl, @Nonnull GlResourceManager glResourceManager) { }
 
     @Override
-    protected void updateDraw(@Nonnull GL2 gl) {
-        assert backgroundRenderer != null : "Update draw should not be called before init draw";
-        backgroundRenderer.setRect(gl, getRect());
-    }
+    protected void updateDraw(@Nonnull GL2 gl) { }
 
     @Override
-    protected void doDraw(@Nonnull GL2 gl, @Nonnull PmvMatrix pmvMatrix) {
-        assert backgroundRenderer != null : "Draw should not be called before init draw";
-        backgroundRenderer.draw(gl, pmvMatrix);
-    }
+    protected void doDraw(@Nonnull GL2 gl, @Nonnull PmvMatrix pmvMatrix) { }
 
     /**
      * Add a widget to this pane.
@@ -98,16 +76,16 @@ public final class VerticalPane extends AbstractPane {
 
     @Override
     protected void updateLayout() {
-        int x = border ? BORDER : 0;
-        int y = border ? BORDER : 0;
-        int width = getSize().getX() - (border ? BORDER * 2 : 0);
+        int x = padding ? PADDING : 0;
+        int y = padding ? PADDING : 0;
+        int width = getSize().getX() - (padding ? PADDING * 2 : 0);
 
         for (IWidget widget : getWidgets()) {
             Vec2i preferredSize = widget.getPreferredSize();
             Vec2i actualSize = new Vec2i(width, preferredSize.getY());
             widget.setSize(actualSize);
             widget.setPosition(new Vec2i(x, y));
-            y += widget.getSize().getY() + PADDING;
+            y += widget.getSize().getY() + SPACING;
         }
     }
 
@@ -125,7 +103,7 @@ public final class VerticalPane extends AbstractPane {
             for (IWidget widget : getWidgets()) {
                 Vec2i preferredSize = widget.getPreferredSize();
                 widget.setSize(preferredSize);
-                height += preferredSize.getY() + PADDING;
+                height += preferredSize.getY() + SPACING;
 
                 if (preferredSize.getX() > width) {
                     width = preferredSize.getX();
@@ -133,22 +111,14 @@ public final class VerticalPane extends AbstractPane {
             }
 
             // Remove the last bit of padding.
-            height -= PADDING;
+            height -= SPACING;
 
-            if (border) {
-                width += 2 * BORDER;
-                height += 2 * BORDER;
+            if (padding) {
+                width += 2 * PADDING;
+                height += 2 * PADDING;
             }
         }
 
         return new Vec2i(width, height);
-    }
-
-    @Override
-    public void remove(@Nonnull GL2 gl) {
-        if (backgroundRenderer != null) {
-            backgroundRenderer.remove(gl);
-        }
-        super.remove(gl);
     }
 }
