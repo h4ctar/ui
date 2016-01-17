72e2ab2e63d40c29af52d1927fd4947b265d01c8
Ben Smith <bensmith87@gmail.com>
2016-01-17 14:05:53 +1100

    Added a set focused widget to the focus manager so that the desktop can focus dialogs that are pushed onto it

--- src/main/java/ben/ui/input/IFocusManager.java
+++ src/main/java/ben/ui/input/IFocusManager.java
@@ -1,5 +1,7 @@
 package ben.ui.input;
 
+import ben.ui.widget.IWidget;
+
 import javax.annotation.Nonnull;
 
 /**
@@ -9,13 +11,22 @@ public interface IFocusManager {
 
     /**
      * Add a listener that will be notified of focus events.
+     *
      * @param focusListener the new listener
      */
     void addFocusListener(@Nonnull IFocusManagerListener focusListener);
 
     /**
      * Remove a focus listener.
+     *
      * @param focusListener the listener to remove
      */
     void removeFocusListener(@Nonnull IFocusManagerListener focusListener);
+
+    /**
+     * Set the currently focused widget.
+     *
+     * @param widget the widget to focus
+     */
+    void setFocusedWidget(@Nonnull IWidget widget);
 }
